5d0199c058ec3ca62102affe6543c6ba26c63134
JYP <bluebinarycode@gmail.com>
2024-06-18 12:04:33 +0900

    GachiPay_init

---
+++ src/main/java/com/gachi/gachipay/GachiPayApplication.java
@@ -0,0 +1,13 @@
+package com.gachi.gachipay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GachiPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GachiPayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gachi/gachipay/GachiPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gachi.gachipay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GachiPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
